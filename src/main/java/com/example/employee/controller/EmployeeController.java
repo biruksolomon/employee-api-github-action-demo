9228909d7c8efe04fc90615b8992cf3b782c141e
Biruk Solomon <biruksolomonmoges@gmail.com>
2026-06-13 01:50:09 +0300

    Pull Request CI + update controller : autowire to service layer

--- src/main/java/com/example/employee/controller/EmployeeController.java
+++ src/main/java/com/example/employee/controller/EmployeeController.java
@@ -1,20 +1,24 @@
 package com.example.employee.controller;
 
 import com.example.employee.model.Employee;
-import org.springframework.web.bind.annotation.*;
+import com.example.employee.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
+@AllArgsConstructor
 public class EmployeeController {
+
+    private final EmployeeService service;
+
 
     @GetMapping
     public List<Employee> getEmployees() {
-
-        return List.of(
-                new Employee(1L, "John"),
-                new Employee(2L, "Alice")
-        );
+        return service.getEmployees();
     }
 }
